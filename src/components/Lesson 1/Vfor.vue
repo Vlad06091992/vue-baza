4d94f23eb4e8e9e35e04f2e87591850c318203fc
Vlad <Smirnov.ru92@mail.ru>
2023-07-25 16:03:38 +0500

    add component with v-for and ref, update array for rendering

--- src/components/Lesson 1/Vfor.vue
+++ src/components/Lesson 1/Vfor.vue
@@ -2,32 +2,35 @@
   <div>v-for_ref</div>
   <button type="button" class="btn btn-primary b" @click="addNum">add number</button>
   <ul class="list-group">
-    <li class="list-group-item" v-for="number in numbers" :key="number">{{ number }}</li>
+    <li @click="double(index)" class="list-group-item" v-for="(number, index) in numbers" :key="number">index : {{index}} /number:{{ number }} </li>
   </ul>
 </template>
 
 //TODO 01.35.00
 
 <script lang="ts">
-import { defineComponent, ref} from "vue";
+import { defineComponent, Ref, ref} from "vue";
 
 export default defineComponent({
   name: "v-for_ref",
   props: {},
   setup() {
-
-    let numbers = ref([1, 2, 3, 4, 5]);
-
+    let numbers:Ref<number[]> = ref([1, 2, 3, 4, 5]);
     const addNum = () => {
-
       let n = Math.floor(Math.random() * 10)
-
       numbers.value.push(n)
+    }
+
+    const double = (i:number) => {
+    // console.log(numbers.value[i])
+
+      numbers.value[i] = numbers.value[i] * 2
     }
 
     return {
       numbers,
-      addNum
+      addNum,
+      double
     };
   }
 });
