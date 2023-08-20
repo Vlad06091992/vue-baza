b4cd4c68f141b49ab5922c0b93f1c6d0bfecb359
Vlad <Smirnov.ru92@mail.ru>
2023-08-20 23:51:29 +0500

    end 2 lesson

--- src/components/Lesson 2/ClassesAndForms.vue
+++ src/components/Lesson 2/ClassesAndForms.vue
@@ -3,17 +3,54 @@
 <template>
   <div>
     <label>
-      <input type="radio" value="orange" :checked="state.color === 'orange'" @change="state.color = 'orange'">orange
+      <input type="radio" value="orange" v-model="state.color">orange
+    </label>
+    <label>
+      <input type="radio" value="red" v-model="state.color">red
     </label>
     <label>
       <input type="radio" value="green" v-model="state.color">green
     </label>
+
+    <label>
+      <input type="radio" value="yellow" v-model="state.color">yellow
+    </label>
     <div>
       state.color:{{ state.color }}
     </div>
+    <div v-for="(val, key) in tableClasses">
+      <label>
+        <input type="checkbox" v-model="tableStyles[key]" />{{key}}
+
+      </label>
+    </div>
+
+
+
+
+
+    <table class="table " :class="tableClasses" >
+      <tr>
+        <td>some</td>
+        <td>some</td>
+        <td>some</td>
+      </tr>
+      <tr>
+        <td>some</td>
+        <td>some</td>
+        <td>some</td>
+      </tr>
+      <tr>
+        <td>some</td>
+        <td>some</td>
+        <td>some</td>
+      </tr>
+
+    </table>
   </div>
 
-  <div :class="state.color">test color</div>
+  <div :class="state.color">test color(state.color)</div>
+  <div :class="computedClasses">test color(computed class return object)</div>
 
 </template>
 
@@ -26,10 +63,34 @@ export default defineComponent({
   components: {},
   setup(props, ctx) {
     const state: any = reactive({
-      color: ""
+      color: "green"
     });
 
-    return { state };
+    let tableStyles = reactive({
+      red: false,
+      hover: false,
+      bordered: false
+    });
+
+    const tableClasses = computed(() => {
+      return {
+        "table-bordered": tableStyles.bordered,
+        "table-hover": tableStyles.hover,
+        "red": tableStyles.red
+      };
+    });
+
+
+    const computedClasses = computed(() => {
+      return {
+        "red": state.color == "red",
+        "green": state.color == "green",
+        "yellow": state.color == "yellow",
+        "orange": state.color == "orange"
+      };
+    });
+
+    return { state, computedClasses, tableClasses, tableStyles };
 
   }
 });
@@ -46,11 +107,23 @@ div {
   margin: 10px;
 }
 
+.main {
+  font-size: 20px;
+}
+
 .orange {
   color: orange;
 }
 
 .green {
   color: green;
+}
+
+.red {
+  color: red
+}
+
+.yellow {
+  color: yellow;
 }
 </style>
